fac276c1c5edc4fed53f17a4d2cab8ba0513462a
shekhar <shekhar@telaverge.com>
2025-01-25 15:29:48 +0530

    Updates

---
+++ precache-manifest.e960d2895f0d715a5b4271e3b883518d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6849b1c911bee4a095ed942fe517410",
+    "url": "/QouteGenerator/index.html"
+  },
+  {
+    "revision": "1c24a020e6850df3c7f2",
+    "url": "/QouteGenerator/static/css/main.1c40610b.chunk.css"
+  },
+  {
+    "revision": "7f2ed8a370c7cb5e22df",
+    "url": "/QouteGenerator/static/js/2.c11b465e.chunk.js"
+  },
+  {
+    "revision": "cb156245ee93a85eed33",
+    "url": "/QouteGenerator/static/js/3.44b60ca4.chunk.js"
+  },
+  {
+    "revision": "1c24a020e6850df3c7f2",
+    "url": "/QouteGenerator/static/js/main.7f449079.chunk.js"
+  },
+  {
+    "revision": "5520535dd68001a79893",
+    "url": "/QouteGenerator/static/js/runtime~main.9615b85a.js"
+  }
+]);
